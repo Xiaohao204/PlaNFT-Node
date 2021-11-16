0f7aca8e9acdef584bc8b5411d6178a5400f4134
pengfeng111833 <429090331@qq.com>
2021-11-16 19:08:52 +0800

    update connection

--- config/dbconn.js
+++ config/dbconn.js
@@ -1,18 +1,18 @@
 var mysql = require('mysql');
 
-const host = 'localhost';
-const user= 'root';
-const password= 'bitoffer123';
-const port= '3306';
-const database= 'test';
-
-
-
-// const host = '10.0.0.18';
-// const user= 'bitoffer_db-test';
-// const password= 'BitOffer-test!@#';
+// const host = 'localhost';
+// const user= 'root';
+// const password= 'bitoffer123';
 // const port= '3306';
-// const database= 'planft';
+// const database= 'test';
+
+
+
+const host = '10.0.0.18';
+const user= 'bitoffer_db-test';
+const password= 'BitOffer-test!@#';
+const port= '3306';
+const database= 'planft';
 
 // connect mysql
 function getConn() {
