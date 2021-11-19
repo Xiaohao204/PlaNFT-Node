a24ac953742723c3dc1413f991055d84f6505261
pengfeng111833 <429090331@qq.com>
2021-11-19 19:09:27 +0800

    update network

--- config/constants.js
+++ config/constants.js
@@ -19,8 +19,8 @@ const pool = mysql.createPool(config);
 module.exports = {
     // network: "https://rinkeby.infura.io/v3/25b55e1839df4d1b977c62bf7b0f35ab",//xinpeng
     // network: "https://rinkeby.infura.io/v3/81f13e5096e4405892775775d811e984",//my
-    network: "https://rinkeby.infura.io/v3/3d1c157ad26247b3bd2b9ae1762b7a05",//simin 
-    // network: "http://10.0.0.89:18545",
+    // network: "https://rinkeby.infura.io/v3/3d1c157ad26247b3bd2b9ae1762b7a05",//simin 
+    network: "http://10.0.0.89:18545",
     eth_contract: {
         Pla_TNFT: "0x3c4AbE3bF4b15046e5DaF238af0bc4d5B7E75463"
     },
