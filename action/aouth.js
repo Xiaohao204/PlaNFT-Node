e0a511fcdb56113c68bdc42d71f8b327e2fc7577
pengfeng111833 <429090331@qq.com>
2021-11-23 20:11:07 +0800

    no message

--- action/aouth.js
+++ action/aouth.js
@@ -8,6 +8,7 @@ var request = require('request');
 const https = require('https')
 
 // exports.ethercanLog = function (req, res) {    //获取ethercan上交易记录信息
+async function actionEthercanLog(req, res, callback) {
 async function getmateData(req, res, callback) {
     request.get(
         {
