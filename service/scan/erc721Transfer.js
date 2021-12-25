45aff591012fc5b2c53836d5c690d4572e3b9725
xinpeng Liao <liaoxinpeng@bitoffer.com>
2021-12-25 16:36:06 +0800

    1

--- service/scan/erc721Transfer.js
+++ service/scan/erc721Transfer.js
@@ -56,6 +56,7 @@ async function scanTransfer(contractAddressList, chainBlockNumber, chain_symbol)
                                 is_frozen: 1,
                                 tokenURI: null,
                                 data: null,
+                                type: 3,
                                 chain_symbol
                             }
                             let tokenURI = await contract.tokenURI(tokenId);
@@ -77,6 +78,9 @@ async function scanTransfer(contractAddressList, chainBlockNumber, chain_symbol)
                                             nftInfoData.title = metadata.name !== undefined ? metadata.name : contract_name + " #" + tokenId;
                                             nftInfoData.tokenURI = tokenURI;
                                             nftInfoData.data = metadata.toString();
+                                            if (nftInfoData.animationUrl !== null) {
+                                                nftInfoData.type = 4;
+                                            }
                                             await insertTransaction(nftInfoData);
                                         }
                                     })
