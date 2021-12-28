fc2153b95476279e3b3cda8f3c6859577b5b3589
xinpeng Liao <liaoxinpeng@bitoffer.com>
2021-12-28 14:21:34 +0800

    update

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,7 @@
 module.exports = {
     ETH: {
         network: [
+            // "https://apis.ankr.com/3bbbc588da014966a07c359568e245dd/66a8bc6c1da7d8668848b016d04675e9/eth/fast/rinkeby",
             "https://speedy-nodes-nyc.moralis.io/cd647532dcdc3ae204954045/eth/rinkeby",
             "https://speedy-nodes-nyc.moralis.io/2b63b620cc2e2abc12e05a38/eth/rinkeby",
             "https://speedy-nodes-nyc.moralis.io/4d5489941eb87ba0e290c179/eth/rinkeby",
@@ -12,6 +13,7 @@ module.exports = {
     },
     BSC: {
         network: [
+            // "https://apis.ankr.com/2e648929ed5e48abb5161629f686c0ad/66a8bc6c1da7d8668848b016d04675e9/binance/full/test",
             "https://speedy-nodes-nyc.moralis.io/cd647532dcdc3ae204954045/bsc/testnet",
             "https://speedy-nodes-nyc.moralis.io/2b63b620cc2e2abc12e05a38/bsc/testnet",
             "https://speedy-nodes-nyc.moralis.io/4d5489941eb87ba0e290c179/bsc/testnet",
@@ -23,6 +25,7 @@ module.exports = {
     },
     POLYGON: {
         network: [
+            // "https://apis.ankr.com/3f42748eae2048929de50dc32a008b62/66a8bc6c1da7d8668848b016d04675e9/polygon/full/test",
             "https://speedy-nodes-nyc.moralis.io/cd647532dcdc3ae204954045/polygon/mumbai",
             "https://speedy-nodes-nyc.moralis.io/2b63b620cc2e2abc12e05a38/polygon/mumbai",
             "https://speedy-nodes-nyc.moralis.io/4d5489941eb87ba0e290c179/polygon/mumbai",
@@ -40,6 +43,7 @@ module.exports = {
             "https://speedy-nodes-nyc.moralis.io/6729fc0fc0c3d8a7f6ee4b00/arbitrum/testnet",
             "https://speedy-nodes-nyc.moralis.io/6d256ae54483df82a3782391/arbitrum/testnet",
             "https://speedy-nodes-nyc.moralis.io/6bd8d0732c1d9c3760f1d506/arbitrum/testnet"
+            // "https://apis.ankr.com/05ea94119be042ac86c261d195eef823/66a8bc6c1da7d8668848b016d04675e9/arbitrum/full/test"
         ],
         chain_symbol: 'ARBITRUM',
     },
