6fb37043cc0e6766bd30c27c90b4b38484f95752
xinpeng Liao <liaoxinpeng@bitoffer.com>
2022-01-20 15:57:28 +0800

    fix

--- config/constants.js
+++ config/constants.js
@@ -2,37 +2,65 @@ module.exports = {
     ETH: {
         network: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
         chain_symbol: 'ETH',
-        contractList: []
+        contractList: [
+            '0x3ee19f477FF16135aeFEA199c238Ded59d065Fd2',
+            '0x8a099fC80cBba4613484219892eb948bA6D23F86',
+            '0xBC19157DfeFa18bAb7AF96d020a6f05bAcD8953C'
+        ]
     },
     BSC: {
         network: "https://data-seed-prebsc-1-s1.binance.org:8545/",
         chain_symbol: 'BSC',
-        contractList: []
+        contractList: [
+            '0x3ee19f477FF16135aeFEA199c238Ded59d065Fd2',
+            '0x8a099fC80cBba4613484219892eb948bA6D23F86',
+            '0xBC19157DfeFa18bAb7AF96d020a6f05bAcD8953C'
+        ]
     },
     POLYGON: {
         network: "https://rpc-mumbai.maticvigil.com/",
         chain_symbol: 'POLYGON',
-        contractList: []
+        contractList: [
+            '0x3ee19f477FF16135aeFEA199c238Ded59d065Fd2',
+            '0x8a099fC80cBba4613484219892eb948bA6D23F86',
+            '0xBC19157DfeFa18bAb7AF96d020a6f05bAcD8953C'
+        ]
     },
     ARBITRUM: {
         network: "https://rinkeby.arbitrum.io/rpc",
         chain_symbol: 'ARBITRUM',
-        contractList: []
+        contractList: [
+            '0x3ee19f477FF16135aeFEA199c238Ded59d065Fd2',
+            '0x8a099fC80cBba4613484219892eb948bA6D23F86',
+            '0xBC19157DfeFa18bAb7AF96d020a6f05bAcD8953C'
+        ]
     },
     FANTOM: {
         network: "https://rpc.testnet.fantom.network/",
         chain_symbol: 'FANTOM',
-        contractList: []
+        contractList: [
+            '0x3ee19f477FF16135aeFEA199c238Ded59d065Fd2',
+            '0x8a099fC80cBba4613484219892eb948bA6D23F86',
+            '0xBC19157DfeFa18bAb7AF96d020a6f05bAcD8953C'
+        ]
     },
     AVAX: {
         network: "https://api.avax-test.network/ext/bc/C/rpc",
         chain_symbol: 'AVAX',
-        contractList: []
+        contractList: [
+            '0x3ee19f477FF16135aeFEA199c238Ded59d065Fd2',
+            '0x8a099fC80cBba4613484219892eb948bA6D23F86',
+            '0xBC19157DfeFa18bAb7AF96d020a6f05bAcD8953C'
+        ]
     },
     AURORA: {
         network: "https://testnet.aurora.dev",
         chain_symbol: 'AURORA',
-        contractList: []
+        contractList: [
+            '0x3ee19f477FF16135aeFEA199c238Ded59d065Fd2',
+            '0x8a099fC80cBba4613484219892eb948bA6D23F86',
+            '0xBC19157DfeFa18bAb7AF96d020a6f05bAcD8953C'
+        ]
     },
     ipfs: {
         main: "https://ipfs.io/ipfs/",
